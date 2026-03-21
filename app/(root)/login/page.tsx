23f57a972ba4f06c975ad1656c36882d009a9233
Rakincev Daniil <rakincevdaniil18@gmail.com>
2026-03-21 21:17:55 +0500

    fix login

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -1,5 +1,10 @@
 import { Login } from "@/shared/components/shared/login/login";
+import { Suspense } from "react";
 
 export default function LoginPage() {
-  return <Login />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  )
 }
